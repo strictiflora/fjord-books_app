d2d58a10692c92de1d7dcc47d271dea243b182fd
kino <himekiyoto@gmail.com>
2021-12-13 20:45:06 +0900

    modify paging function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,7 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @users = User.all.order(:id).page(params[:page]).per(3)
+    @users = User.all.order(:id).page(params[:page])
   end
 
   def show
