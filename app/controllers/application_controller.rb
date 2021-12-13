2151ff16b1cea55d17fd1c6f070e0fa740b3110d
kino <himekiyoto@gmail.com>
2021-12-13 21:17:01 +0900

    Correct offenses following rubocop

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:zip_code, :address, :self_introduction])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[zip_code address self_introduction])
   end
 end
--- db/migrate/20211213010534_devise_create_users.rb
+++ db/migrate/20211213010534_devise_create_users.rb
@@ -4,10 +4,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
-      t.string :zip_code,           null: false, default: ""
-      t.string :address,            null: false, default: ""
+      t.string :email,              null: false, default: ''
+      t.string :encrypted_password, null: false, default: ''
+      t.string :zip_code,           null: false, default: ''
+      t.string :address,            null: false, default: ''
       t.text :self_introduction
 
       ## Recoverable
@@ -34,7 +34,6 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
       # t.integer  :failed_attempts, default: 0, null: false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
-
 
       t.timestamps null: false
     end
