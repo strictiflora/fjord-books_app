753e5d8454a022d2e7412d8b92b713295e10bfec
kino <himekiyoto@gmail.com>
2021-12-13 15:49:34 +0900

    Add the paging function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
-  
+
   def index
-    @users = User.all
+    @users = User.all.order(:id).page(params[:page]).per(3)
   end
 
   def show
