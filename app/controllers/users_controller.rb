c5739e085162d9a4a695111d17cd2bb2ad756c0c
kino <himekiyoto@gmail.com>
2021-12-13 20:36:41 +0900

    Add some input fields to sign-up and user-edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  layout 'devise'
+
   before_action :authenticate_user!
 
   def index
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/index'
-  get 'users/show'
   root to: 'books#index'
   devise_for :users
   resources :users, :only => [:index, :show]
   resources :books
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
