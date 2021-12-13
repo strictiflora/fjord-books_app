5f90f4cdb4ac150692adea598c4a591d40523b28
kino <himekiyoto@gmail.com>
2021-12-13 15:40:17 +0900

    Add user_index and showing detail page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/index'
+  get 'users/show'
   root to: 'books#index'
+  devise_for :users
+  resources :users, :only => [:index, :show]
   resources :books
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
